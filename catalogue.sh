ed429910a24d392fc13ce54a4fe23b0be7cae902
suresh <thesureshvadde@gmail.com>
2023-07-10 22:24:41 +0530

    implementation

--- catalogue.sh
+++ catalogue.sh
@@ -2,46 +2,61 @@ source common.sh
 
 print_head "Configuring NodeJS Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
 
 print_head " Install NodeJS"
 yum install nodejs -y &>>${log_file}
+status_check $?
 
 print_head "Creating Roboshop User"
 useradd roboshop &>>${log_file}
+status_check $?
 
 print_head "Creating Application Directory"
 mkdir /app &>>${log_file}
+status_check $?
 
 print_head "Removing Old Content"
 rm -rf /app/* &>>${log_file}
+status_check $?
 
 print_head "Downloading Catalogue Content"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+status_check $?
 cd /app
 
 print_head "Extracting Catalogue Content"
 unzip /tmp/catalogue.zip &>>${log_file}
+status_check $?
 
 print_head "Installing NodeJS dependencies"
 npm install &>>${log_file}
+status_check $?
 
 print_head "Copy SystemD service file"
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+status_check $?
 
 print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
+status_check $?
 
 print_head "Enable Catalogue Service"
 systemctl enable catalogue &>>${log_file}
+status_check $?
 
 print_head "Start Catalogue Service"
 systemctl restart catalogue &>>${log_file}
+status_check $?
 
 print_head "Copy mongodb repo file"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+status_check $?
 
 print_head "Install mongo clint"
 yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
 
 print_head "load schema"
 mongo --host mongodb.sureshdevops.online </app/schema/catalogue.js &>>${log_file}
+status_check $?
--- mongodb.sh
+++ mongodb.sh
@@ -2,17 +2,21 @@ source common.sh
 
 print_head "set of mongodb repository"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
 
 print_head "Installing mongodb"
 yum install mongodb-org -y &>>${log_file}
+status_check $?
 
 print_head "Upadte Mongodb listen Address"
 sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf &>>${log_file} # search for 127.0.0.0 and update with 0.0.0.0
+status_check $?
 
 print_head "enabling mongodb"
 systemctl enable mongod &>>${log_file}
+status_check $?
 
 print_head "start mongodb service"
 systemctl restart mongod &>>${log_file}
-
+status_check $?
 
