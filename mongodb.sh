36a299bd1a9b1fc7326e39257880438498e4a9a2
suresh <thesureshvadde@gmail.com>
2023-07-10 21:53:13 +0530

    implementing functions

--- mongodb.sh
+++ mongodb.sh
@@ -6,12 +6,13 @@ cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 print_head "Installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 
+print_head "Upadte Mongodb listen Address"
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf &>>${log_file} # search for 127.0.0.0 and update with 0.0.0.0
+
 print_head "enabling mongodb"
 systemctl enable mongod &>>${log_file}
 
 print_head "start mongodb service"
-systemctl start mongod &>>${log_file}
-
 systemctl restart mongod &>>${log_file}
 
-#update /etc/mongodb.conf file from 127.0.0.1 with 0.0.0.0
+
