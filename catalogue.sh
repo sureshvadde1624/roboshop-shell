1dcfb9d7127569b53c09956dc9016993f33a0ff1
suresh <thesureshvadde@gmail.com>
2023-07-10 21:38:52 +0530

    implementing functions

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,47 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+source common.sh
+
+print_head "Configuring NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head " Install NodeJS"
+yum install nodejs -y &>>${log_file}
+
+print_head "Creating Roboshop User"
+useradd roboshop &>>${log_file}
+
+print_head "Creating Application Directory"
+mkdir /app &>>${log_file}
+
+print_head "Removing Old Content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading Catalogue Content"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "Extracting Catalogue Content"
+unzip /tmp/catalogue.zip &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print_head "Installing NodeJS dependencies"
+npm install &>>${log_file}
 
-mongo --host mongodb.sureshdevops.online </app/schema/catalogue.js
+print_head "Copy SystemD service file"
+cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue Service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "Start Catalogue Service"
+systemctl restart catalogue &>>${log_file}
+
+print_head "Copy mongodb repo file"
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Install mongo clint"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "load schema"
+mongo --host mongodb.sureshdevops.online </app/schema/catalogue.js &>>${log_file}
--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,17 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
+source common.sh
+
+print_head "set of mongodb repository"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+print_head "Installing mongodb"
+yum install mongodb-org -y &>>${log_file}
+
+print_head "enabling mongodb"
+systemctl enable mongod &>>${log_file}
+
+print_head "start mongodb service"
+systemctl start mongod &>>${log_file}
+
+systemctl restart mongod &>>${log_file}
+
+#update /etc/mongodb.conf file from 127.0.0.1 with 0.0.0.0
