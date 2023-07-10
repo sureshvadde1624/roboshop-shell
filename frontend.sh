abe8ec2695bc442c3992b24ed647f249929ea180
suresh <thesureshvadde@gmail.com>
2023-07-10 18:18:02 +0530

    print statements in frontend

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,22 @@
+echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
+
+echo -e "\e[35mRemoving Old Content\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[35mDownloading Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[35mExtrating Downloaded Frontend Content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[35mCoping nginx Config for Roboshop\e[0m"
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[35menabling nginx\e[0m"
 systemctl enable nginx
+
+echo -e "\e[35mstarting nginx\e[0m"
 systemctl restart nginx
 
