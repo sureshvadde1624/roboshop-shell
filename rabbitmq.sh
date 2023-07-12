362affd8c18437036b3b0bf0fe8efbee0dd2d35f
suresh <thesureshvadde@gmail.com>
2023-07-12 07:57:13 +0530

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,19 +2,13 @@ source
 
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then # -z checks var empty or not if empty it throws TRUE.
-  echo -e "\e[31mMissing mysql root password argument\e[0m"
+  echo -e "\e[31mMissing rabbitmq root password argument\e[0m"
   exit 1
 fi
 
 print_head "setup Erlang repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log_file}
 status_check $?
-
-roboshop_app_password=$1
-if [ -z "${roboshop_app_password}" ]; then # -z checks var empty or not if empty it throws TRUE.
-  echo -e "\e[31mMissing mysql root password argument\e[0m"
-  exit 1
-fi
 
 print_head " setup rabbitmq repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
@@ -33,7 +27,10 @@ systemctl restart rabbitmq-server &>>${log_file}
 status_check $?
 
 print_head "add application user"
-rabbitmqctl add_user roboshop ${roboshop_app_password} &>>${log_file}
+rabbitmqctl list_users | grep roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop ${roboshop_app_password} &>>${log_file}
+fi
 status_check $?
 
 print_head "configure permissions for application user"
